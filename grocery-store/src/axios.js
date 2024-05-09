b67f0ab24dde54749388aca5c23a7caf87ec6d2f
Richa-9 <109239645+Richa-9@users.noreply.github.com>
2024-05-09 23:37:35 +0530

    Delete grocery-store directory

--- grocery-store/src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  // THE API (cloud function) URL
-  baseURL: 'http://127.0.0.1:5001/challenge-d6c56/us-central1/api'
-});
-
-export default instance;
-
